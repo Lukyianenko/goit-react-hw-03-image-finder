4579552f5d665f471a18c5ca289f886483901de3
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-02-28 18:55:34 +0200

    add search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import Searchbar from './SearchImg/Searchbar';
+import css from './SearchImg/styles.module.css';
+
+// const API_KEY = '33017005-3089560dbf89e85a2a48421c2';
+
+class App extends Component {
+  state = {
+    searchName: '',
+  }
+
+  onSubmitSaveName = (search) => {
+    const normilizedName = search.name.toLowerCase().trim();
+    if(normilizedName === '') {
+      alert('Please, enter a search name');
+      return
+    }
+    this.setState({
+      searchName: normilizedName
+    })
+  }
+
+  render() {
+    return (
+      <div className={css.App}>
+
+  <Searchbar onSubmit={this.onSubmitSaveName}/>
+      </div>
+    );
+  }
+  
 };
+
+export default App;
